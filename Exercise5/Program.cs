80c90f29222f39059912f585ada811b90a2984fc
Conor Koritor <ckoritor@gmail.com>
2024-01-29 10:43:26 +0000

    Exercise 5 complete

---
+++ Exercise5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo[] files = new DirectoryInfo(@"C:\\Windows").GetFiles();
+
+            var query = files
+                .Where(f => f.Length > 1000)
+                .OrderBy(f => f.Length)
+                .Select(f => new
+                {
+                    Name = f.Name,
+                    Length = f.Length,
+                    CreationTime = f.CreationTime
+                });
+            Console.WriteLine("File Name\tSize\t\tCreation Date");
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(
+                    "{0} \t{1} bytes, \t{2}",
+                    item.Name, item.Length, item.CreationTime.ToShortDateString()
+                    );
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
