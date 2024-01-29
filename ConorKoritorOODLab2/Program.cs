6c7b7793a7377a250f66fdfedb20110a4e2def66
Conor Koritor <ckoritor@gmail.com>
2024-01-29 10:23:09 +0000

    Add project files.

---
+++ ConorKoritorOODLab2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConorKoritorOODLab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = { 1, 5, 3, 6, 11, 2, 15, 21, 13, 12, 10 };
+
+            var outputNumbers = from number in numbers
+                                where number > 5
+                                orderby number
+                                select number;
+
+            foreach( var number in outputNumbers )
+            {
+                Console.WriteLine( number.ToString() );
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Exercise3/MyFileInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Exercise3
+{
+    internal class MyFileInfo
+    {
+        public string Name { get; set; }
+        public long Length {  get; set; }
+        public DateTime CreationTime {  get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0,-40}{1,6:F0} MB {2}", Name, Length / 1000, CreationTime.ToShortDateString());
+        }
+    }
+}
