8b39727af7d087c2d9f7888deaad29b97bcbb7b1
Conor Koritor <ckoritor@gmail.com>
2024-01-29 10:49:05 +0000

    Completed Exercise 6

---
+++ Exercise6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    internal class Program
+    {
+        private static int DoubleIt(int number)
+        {
+            Console.WriteLine("About to Double the number " + number.ToString());
+            return number * 2;
+        }
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 5, 3, 6, 10, 12, 8 };
+
+            var query = numbers
+                .Select(n => DoubleIt(n));
+            Console.WriteLine("Before the foreach Loop");
+            foreach(var item in query)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
