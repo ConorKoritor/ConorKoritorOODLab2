55d12ec630cf9380807884ac2d565045d63cd9a2
Conor Koritor <ckoritor@gmail.com>
2024-01-29 11:14:00 +0000

    Completed Exercise 8

---
+++ Exercise8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Mary", "Joseph", "Michael", "Sarah", "Margaret", "John" };
+
+            var query = names
+                .Where(n => n.Length > 4)
+                .Select(n => n);
+
+            foreach( var name in query)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
