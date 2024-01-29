099512c00edb92dd7222daa22693e165421a8c40
Conor Koritor <ckoritor@gmail.com>
2024-01-29 10:31:29 +0000

    Exercise 3 Completed

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
 
 namespace Exercise3
 {
@@ -10,6 +12,25 @@ namespace Exercise3
     {
         static void Main(string[] args)
         {
+            FileInfo[] files = new DirectoryInfo(@"c:/windows").GetFiles();
+
+            var query = from item in files
+                        where item.Length > 1000
+                        orderby item.Length descending, item.Name
+                        select new MyFileInfo
+                        {
+                            Name = item.Name,
+                            Length = item.Length,
+                            CreationTime = item.CreationTime
+                        };
+            Console.WriteLine(String.Format("{0,-40}{1,6:F0} MB {2}", "Filenames", "Size", "Creation Date"));
+
+            foreach(MyFileInfo item in query)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
         }
     }
 }
